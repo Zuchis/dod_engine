20f029e368d8ebb97116746a44348c4fb98b2377
Zuchis <liquimaster@gmail.com>
2017-10-30 08:52:53 +0000

    finished the test, not working unfortunately

--- engine.h
+++ engine.h
@@ -1,7 +1,19 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <SFML/OpenGL.hpp>
+#include <GL/glew.h>
 
 #include "math/math.h"
 #include "visual/visual.h"
+
+namespace engine {
+    const unsigned int VCOORDS = 0;
+    const unsigned int TCOORDS = 1;
+    const unsigned int NCOORDS = 2;
+
+    GLuint UBO_BP = 0;
+
+    float cameraDistance = 15;
+
+    void engine_init(void);
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 #include "engine.h"
 
+void engine_init(void) {
+    //SHADERS Init
+
+    //GLEW Init
+    glewExperimental = GL_TRUE;
+    GLenum result = glewInit();
+
+    if (result != GLEW_OK) 
+    {
+        std::cerr << "ERROR glewInit: " << glewGetString(result) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    GLenum err_code = glGetError();
+}
+
 int main()
 {
-    sf::Window window(sf::VideoMode(1024, 768), "Sick Engine M8");
+    unsigned int width = 1024;
+    unsigned int height = 768;
+
+    math::Quaternion rotationQuaternion(1.0f,0.0f,0.0f,0.0f);
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "Sick Engine M8");
+
+    engine_init();
+
+    ShaderProgram program = ShaderProgram();
+    program.compileShaderFromFile("./data/shaders/vertexShaderSceneManagement.glsl", ShaderType::VERTEX);
+    program.compileShaderFromFile("./data/shaders/fragmentShaderSceneManagement.glsl", ShaderType::FRAGMENT);
+    program.bindAttribLocation(engine::VCOORDS, "in_Position");
+    program.bindAttribLocation(engine::TCOORDS, "in_TexCoords");
+    program.bindAttribLocation(engine::NCOORDS, "in_Normal");
+    program.addUniformBlock("Camera", engine::UBO_BP);
+    program.link();
+
+    ArcballCamera camera = ArcballCamera(engine::UBO_BP);
+    camera.setProjectionMatrix(math::Perspective(30.0f, width / height, 0.1f, 100.0f));
+
+    //TODO move this one to the main loop later
+    Matrix4 translation = math::translate(Vector3(0.0f,0.0f,engine::cameraDistance * -1));
+    Matrix4 rotation    = rotationQuaternion.toMatrix();
+    camera.setViewMatrix(translation * rotation);
+
+    Matrix4 modelMatrix = math::translate(Vector3(0.0f,0.0f,0.0f)) *
+                            math::Quaternion(0.0f,Vector3(-1.0f,0.0f,0.0f)).toMatrix() *
+                            math::scale(Vector3(4.0f,3.0f,0.4f));
+                            
+    //Matrix4 modelMatrix = math::Create4DIdentity();
+    
+    
+
+    std::string tamSquare("objects/tamSquare.obj");
+    engine::Mesh cube(tamSquare);
+    cube.create();
 
     while (window.isOpen())
     {
+        // Treat the events 
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -16,7 +70,15 @@ int main()
             }
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Drawing goes here
+        
+
+        program.use();
+        program.setUniform("Matrix", modelMatrix.getData());
+
+        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        cube.draw();
 
         window.display();
     }
